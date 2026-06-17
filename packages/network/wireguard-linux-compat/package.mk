6fb45dcac806fc7708f7882276483cbb1c985285
bxgms <542344410@qq.com>
2026-06-17 16:46:38 +0800

    build 17

--- packages/network/wireguard-linux-compat/package.mk
+++ packages/network/wireguard-linux-compat/package.mk
@@ -4,7 +4,8 @@
 
 PKG_NAME="wireguard-linux-compat"
 PKG_VERSION="1.0.20220627"
-PKG_SHA256="362d412693c8fe82de00283435818d5c5def7f15e2433a07a9fe99d0518f63c0"
+# PKG_SHA256="362d412693c8fe82de00283435818d5c5def7f15e2433a07a9fe99d0518f63c0"
+PKG_SHA256="19b181e5c7d2260c23ecad4ad324425fd8e88200d97a885a2c7d4bd26cd61461"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://www.wireguard.com"
 PKG_URL="https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-$PKG_VERSION.tar.xz"
